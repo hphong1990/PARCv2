d59504045e5a2a054bfde12c22708bdf2e575212
hphong1990 <phongnch90@gmail.com>
2024-01-18 12:37:22 -0500

    update newest changes

---
+++ parc/.ipynb_checkpoints/layer-checkpoint.py
@@ -0,0 +1,320 @@
+from tensorflow import keras
+from tensorflow.keras import  layers, regularizers
+from keras.layers import *
+import tensorflow as tf
+
+def resnet_unit(feat_dim, kernel_size, x_in):
+    """
+    Resnet unit: x_in --> conv --> relu --> conv --> + relu --> x_out
+                    |                           |
+                     ---------------------------
+    Parameter: 
+                - feat_dim (int): number of channels
+                - kernel_size (int): size of convolution kernel
+                - x_in (tensor): input tensor
+    Return:
+                - (tensor): output tensor of residual unit.
+    """
+    res = keras.Sequential([
+        Conv2D(feat_dim,
+               kernel_size, padding="same"),
+        ReLU(),
+        Conv2D(feat_dim,
+               kernel_size,
+               padding="same")
+    ])
+    return ReLU()(x_in + res(x_in))
+
+def resnet_block(x_in, feat_dim, kernel_size, reps, pooling = True):
+    """
+    Assembly of multiple resnet unit: x_in --> 2 x (conv + relu) --> 'reps' x resnet_unit --> output
+    Parameter: 
+                - x_in (tensor): input tensor
+                - feat_dim (int): number of channels
+                - kernel_size (int): size of convolution kernel
+                - reps (int): number of residual unit
+                - pooling (bool): the block ends with pooling or not
+    Return:
+                - (tensor): output of the resnet block
+    """
+    conv1 = Conv2D(feat_dim,
+                   kernel_size,
+                   padding="same")(x_in)
+    relu1 = ReLU()(conv1)
+    conv2 = Conv2D(feat_dim,
+                   kernel_size,
+                   padding="same")(relu1)
+    x = ReLU()(conv2)
+    for _ in range(reps):
+        x = resnet_unit(feat_dim,kernel_size,x)
+    if pooling == True:
+        x = MaxPooling2D(2,2)(x)
+        return x
+    else:
+        return x
+
+def conv_unit(feat_dim, kernel_size, x):
+    x = Conv2D(feat_dim, 
+               kernel_size, 
+               activation = LeakyReLU(0.2), 
+               padding="same")(x)
+    x = Conv2D(feat_dim,
+               1,
+               activation = LeakyReLU(0.2),
+               padding="same")(x)
+    return x
+
+def conv_block_down(x, feat_dim, reps, kernel_size, mode = 'normal'):
+    if mode == 'down':
+        x = MaxPooling2D(2,2)(x)
+    for _ in range(reps):
+        x = conv_unit(feat_dim, 
+                      kernel_size,
+                      x)
+    return x
+
+def conv_block_up_w_concat(x, x1, feat_dim, reps, kernel_size, mode = 'normal'):
+    if mode == 'up':
+        x = UpSampling2D((2,2),interpolation='bilinear')(x)
+    
+    x = Concatenate()([x,x1])
+    for _ in range(reps):
+        x = conv_unit(feat_dim,
+                      kernel_size,
+                      x)
+    return x
+
+def conv_block_up_wo_concat(x, feat_dim, reps, kernel_size, mode = 'normal'):
+    if mode == 'up':
+        x = UpSampling2D((2,2),interpolation='bilinear')(x)
+    for _ in range(reps):
+        x = conv_unit(feat_dim,
+                      kernel_size,
+                      x)
+    return x
+
+class SPADE(layers.Layer):
+    def __init__(self, filters, epsilon=1e-5, **kwargs):
+        super().__init__(**kwargs)
+        self.epsilon = epsilon
+        self.conv = layers.Conv2D(filters, 3, padding="same", activation="relu")
+        self.conv_gamma = layers.Conv2D(filters, 3, padding="same")
+        self.conv_beta = layers.Conv2D(filters, 3, padding="same")
+
+    def build(self, input_shape):
+        self.resize_shape = input_shape[1:3]
+        # print(self.resize_shape)
+
+    def call(self, input_tensor, raw_mask):
+        with tf.device('/GPU:0'):
+
+            mask = tf.image.resize(raw_mask, self.resize_shape, method="nearest")
+            x = self.conv(mask)    
+
+            gamma = self.conv_gamma(x)
+            beta = self.conv_beta(x)
+            mean, var = tf.nn.moments(input_tensor, axes=(0, 1, 2), keepdims=True)
+            std = tf.sqrt(var + self.epsilon)
+
+            normalized = (input_tensor - mean) / std
+            output = gamma * normalized + beta
+            
+        return output
+
+def spade_generator_unit(x, mask, feats_out, kernel, upsampling = True):
+    x = GaussianNoise(0.05)(x)
+    
+    # Residual SPADE & conv
+    spade1 = SPADE(feats_out)(x, mask)
+    relu1 = LeakyReLU(0.2)(spade1)
+    conv1 = Conv2D(feats_out,kernel, padding='same')(relu1)
+    spade2 = SPADE(feats_out)(conv1, mask)
+    relu2 = LeakyReLU(0.2)(spade2)
+    conv2 = Conv2D(feats_out,kernel, padding='same')(relu2)
+    
+    # Skip
+    spade_skip = SPADE(feats_out)(x, mask)
+    relu_skip = LeakyReLU(0.2)(spade_skip)
+    conv_skip = Conv2D(feats_out,kernel, padding='same')(relu_skip)
+
+    # Add 
+    output = tf.keras.layers.add([conv_skip,conv2])
+    if upsampling == True:
+        output = UpSampling2D(size = (2,2),interpolation='bilinear')(output)
+        return output
+    else:
+        return output
+
+def feature_extraction_unet(input_shape = (128,192), n_out_features = 128, n_base_features = 64, n_channel = 5):
+    inputs = keras.Input(shape = (input_shape[0], input_shape[1],n_channel))
+
+    conv1 = conv_block_down(inputs,
+                            feat_dim = n_base_features,
+                            reps = 1,
+                            kernel_size = 3)
+    conv2 = conv_block_down(conv1,
+                            feat_dim = n_base_features*2,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'down')
+    conv3 = conv_block_down(conv2,
+                            feat_dim = n_base_features*4,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'down')
+    conv4 = conv_block_down(conv3,
+                            feat_dim = n_base_features*8,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'down')
+    conv5 = conv_block_down(conv4,
+                            feat_dim = n_base_features*16,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'down')
+    conv6 = conv_block_up_wo_concat(conv5,
+                                    feat_dim = n_base_features*8,
+                                    reps = 1,
+                                    kernel_size = 3,
+                                    mode = 'up')
+    conv7 = conv_block_up_w_concat(conv6, conv3,
+                                    feat_dim = n_base_features*4,
+                                    reps = 1,
+                                    kernel_size = 3,
+                                    mode = 'up')
+    conv8 = conv_block_up_wo_concat(conv7,
+                                    feat_dim = n_base_features*2,
+                                    reps = 1,
+                                    kernel_size = 3,
+                                    mode = 'up')
+    
+    conv9 = conv_block_up_w_concat(conv8, conv1,
+                                    feat_dim = n_out_features,
+                                    reps = 1,
+                                    kernel_size = 3,
+                                    mode = 'up')
+
+    feature_out = conv_block_up_wo_concat(conv9,
+                                    feat_dim = n_out_features,
+                                    reps = 1,
+                                    kernel_size = 1,
+                                    mode = 'normal')
+    unet = keras.Model(inputs, feature_out)
+    return unet
+
+def feature_extraction_burgers(input_shape = (128,192), n_out_features = 64, n_base_features = 64, n_channel = 5):
+    inputs = keras.Input(shape = (input_shape[0], input_shape[1],n_channel))
+    # Need to put normalization layer
+    conv1 = conv_block_down(inputs,
+                            feat_dim = n_base_features,
+                            reps = 1,
+                            kernel_size = 3)
+    conv2 = conv_block_down(conv1,
+                            feat_dim = n_base_features*2,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'down')
+    conv3 = conv_block_down(conv2,
+                            feat_dim = n_base_features*4,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'down')
+    conv8 = conv_block_up_wo_concat(conv3,
+                                    feat_dim = n_base_features*2,
+                                    reps = 1,
+                                    kernel_size = 3,
+                                    mode = 'up')
+    conv9 = conv_block_up_w_concat(conv8, conv1,
+                                    feat_dim = n_out_features,
+                                    reps = 1,
+                                    kernel_size = 3,
+                                    mode = 'up')
+
+    feature_out = conv_block_up_wo_concat(conv9,
+                                    feat_dim = n_out_features,
+                                    reps = 1,
+                                    kernel_size = 1,
+                                    mode = 'normal')
+    unet = keras.Model(inputs, feature_out)
+    return unet
+
+def poisson_block(input_shape = (128,192),n_base_features = 64):
+    inputs = keras.Input(shape = (input_shape[0], input_shape[1], n_base_features), dtype = tf.float32)
+    inputs_2 = keras.Input(shape = (input_shape[0], input_shape[1], 3), dtype = tf.float32)
+    concat = Concatenate(axis = -1)([inputs,inputs_2])
+    conv = conv_block_down(concat,
+                           feat_dim = n_base_features,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'normal')
+    conv_res = resnet_block(conv, n_base_features, kernel_size = 3, reps = 2, pooling = False)
+    conv_out = Conv2D(1,1, padding='same')(conv_res)
+    poisson = keras.Model([inputs,inputs_2], conv_out)
+    return poisson
+
+class Advection(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, state_variable, velocity_field):
+        dy, dx = tf.image.image_gradients(state_variable)
+        spatial_deriv = tf.concat([dy,dx],axis = -1)
+        advect = tf.reduce_sum(tf.multiply(spatial_deriv,velocity_field),axis = -1, keepdims=True)
+        return advect
+
+class Diffusion(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, state_variable):
+        dy, dx = tf.image.image_gradients(state_variable)
+        dyy, dyx = tf.image.image_gradients(dy)
+        dxy, dxx = tf.image.image_gradients(dx)
+        laplacian = tf.add(dyy,dxx)
+        return laplacian
+    
+class Poisson(layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, vector_field):
+        uy, ux = tf.image.image_gradients(vector_field[0])
+        vy, vx = tf.image.image_gradients(vector_field[1])
+        ux2 = tf.multiply(ux,ux)
+        vy2 = tf.multiply(vy,vy)
+        uyvx = tf.multiply(uy,vx)
+        return ux2,vy2,uyvx
+
+def mapping_and_recon_cnn(input_shape = (128,192), n_base_features = 128, n_mask_channel = 1, output_channel = 1 ):
+    inputs = keras.Input(shape = (input_shape[0], input_shape[1], n_base_features), dtype = tf.float32)
+    inputs_2 = keras.Input(shape = (input_shape[0], input_shape[1], n_mask_channel), dtype = tf.float32)
+    
+    # Style vector 
+    spade_1 = spade_generator_unit(inputs,
+                                   inputs_2,
+                                   n_base_features,
+                                   1,
+                                   upsampling = False)
+    conv_last = resnet_block(spade_1, n_base_features, kernel_size = 1, reps = 2, pooling = False)
+
+    conv_out = Conv2D(output_channel,1, padding='same')(conv_last)
+    mapping_resnet = keras.Model([inputs,inputs_2], conv_out)
+    return mapping_resnet
+
+def integrator_cnn(input_shape = (128,192), n_base_features = 128, n_output = 1):
+    inputs = keras.Input(shape = (input_shape[0], input_shape[1],n_output), dtype = tf.float32)
+    inputs_2 = keras.Input(shape = (input_shape[0], input_shape[1],n_output), dtype = tf.float32)
+
+    conv = resnet_block(inputs, n_base_features, kernel_size = 1, reps = 0, pooling = False)
+
+    spade_1 = spade_generator_unit(conv,
+                                   inputs_2,
+                                   n_base_features,
+                                   1,
+                                   upsampling = False)
+    
+    conv_2 = resnet_block(spade_1, n_base_features, kernel_size = 1, reps = 2, pooling = False)
+    conv4 = Conv2D(n_output, 1, padding = 'same')(conv_2)
+        
+    integrator_resnet = keras.Model([inputs, inputs_2], conv4)
+    return integrator_resnet
---
+++ parc/model/model_ns.py
@@ -0,0 +1,236 @@
+from tensorflow import keras
+from tensorflow.keras import  layers, regularizers
+from keras.layers import *
+import tensorflow as tf
+from parc import layer
+
+from tensorflow.keras.layers import Concatenate, Input
+from tensorflow.keras.models import Model
+
+def differentiator_ns():
+    # Model initiation
+    feature_extraction = layer.feature_extraction_unet(input_shape = (128,256), n_out_features = 64, n_base_features = 64, n_channel = 3)
+    
+    advection = [layer.Advection() for _ in range(2)]
+    diffusion = [layer.Diffusion() for _ in range(2)]
+    velocity_mapping_and_recon = layer.mapping_and_recon_cnn(input_shape = (128,256), n_base_features = 64, n_mask_channel=4, output_channel=2)
+
+    # Main computation graph
+    velocity_field = Input(shape=(128,256, 3), dtype = tf.float32)
+
+    # Reaction term
+    dynamic_feature = feature_extraction(velocity_field)
+
+    # u
+    advec_u = advection[0](velocity_field[:, :, :, 0:1], velocity_field[:, :, :, :2])
+    diffusion_u = diffusion[0](velocity_field[:, :, :, 0:1])
+    
+    # v
+    advec_v = advection[1](velocity_field[:, :, :, 1:2], velocity_field[:, :, :, :2])
+    diffusion_v = diffusion[1](velocity_field[:, :, :, 1:2])    
+    
+    # Concatenate
+    advec_diff_concat = Concatenate(axis=-1)([advec_u,advec_v,diffusion_u,diffusion_v])
+    
+    # Final mapping
+    velocity_dot = velocity_mapping_and_recon([dynamic_feature, advec_diff_concat])
+    
+    differentiator = Model(velocity_field, velocity_dot, name = 'differentiator')
+    return differentiator
+
+
+def poisson_block(input_shape = (128,256),n_base_features = 64):
+    inputs = keras.Input(shape = (input_shape[0], input_shape[1], 3), dtype = tf.float32)
+    poisson = layer.Poisson()([inputs[:,:,:,0:1],inputs[:,:,:,1:2]])
+                               
+    concat = Concatenate(axis = -1)([inputs,poisson[0], poisson[1],poisson[2]])
+    conv = layer.conv_block_down(concat,
+                           feat_dim = n_base_features,
+                            reps = 1,
+                            kernel_size = 3,
+                            mode = 'normal')
+    conv_res = layer.resnet_block(conv, n_base_features, kernel_size = 3, reps = 2, pooling = False)
+    conv_out = Conv2D(1,1, padding='same')(conv_res)
+    poisson = keras.Model([inputs], conv_out,  name = 'possion')
+    return poisson
+ 
+def integrator():
+    state_var_prev = keras.layers.Input(shape = (128, 256, 2), dtype = tf.float32)
+    state_var_dot = keras.layers.Input(shape = (128, 256,2), dtype = tf.float32)
+
+    conv = layer.conv_block_down(state_var_dot,
+                           feat_dim = 64,
+                            reps = 1,
+                            kernel_size = 5,
+                            mode = 'down')
+    conv2 = layer.conv_block_down(conv,
+                           feat_dim = 128,
+                            reps = 1,
+                            kernel_size = 5,
+                            mode = 'normal')
+    conv3 = layer.conv_block_up_wo_concat(conv2,
+                           feat_dim = 64,
+                            reps = 1,
+                            kernel_size = 5,
+                            mode = 'up')
+    conv_out = Conv2D(2,3, padding='same')(conv3)
+    state_var_next = state_var_prev + conv_out 
+    integrator = keras.Model([state_var_dot, state_var_prev], [state_var_next], name = 'integrator')
+    return integrator
+
+@keras.saving.register_keras_serializable()
+class PARCv2_ns(keras.Model):
+    def __init__(self, n_time_step, step_size, solver = "rk4", mode = "integrator_training", use_data_driven_int = True, differentiator_backbone = 'em', **kwargs):
+        super(PARCv2_ns, self).__init__(**kwargs)
+        self.n_time_step = n_time_step
+        self.step_size = step_size
+        self.solver = solver
+        self.mode = mode
+        self.use_data_driven_int = use_data_driven_int
+        self.differentiator = differentiator_ns()
+        self.poisson = poisson_block()
+        self.integrator = integrator()
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        if self.mode == "integrator_training":
+            self.differentiator.trainable = False
+            self.poisson.trainable = False
+        else:
+            self.integrator.trainable = False
+
+    @property
+    def metrics(self):
+        return [
+        self.total_loss_tracker,
+        ]
+    
+    def call(self, input_data):
+        input_seq_current = tf.cast(input_data,dtype = tf.float32)
+        res = []
+        res.append(input_seq_current)
+        if self.use_data_driven_int == True:
+            for ts in range(self.n_time_step):
+                # Compute k1
+                pressure = self.poisson(input_seq_current)
+                input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,:2], pressure])
+                input_seq_current, update = self.explicit_update(input_seq_current)
+                velocity_next = self.integrator([update, input_seq_current[:,:,:,:2]])
+                input_seq_current = Concatenate(axis = -1)([velocity_next, input_seq_current[:,:,:,2:3]])
+                input_seq_current = tf.clip_by_value(input_seq_current,0,1)
+                res.append(input_seq_current[:,:,:,:3])
+            output = Concatenate(axis = -1)(res)
+                
+        else:
+            for ts in range(self.n_time_step):
+                pressure = self.poisson(input_seq_current)
+                input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,:2], pressure])
+                input_seq_current, update = self.explicit_update(input_seq_current)
+                input_seq_current = tf.clip_by_value(input_seq_current,0,1)
+                res.append(input_seq_current[:,:,:,:3])
+            output = Concatenate(axis = -1)(res)      
+        return output
+
+    @tf.function
+    def train_step(self, data):
+        velocity_init = tf.cast(data[0], dtype = tf.float32)
+        velocity_gt = tf.cast(data[1], dtype = tf.float32)
+
+        input_seq_current = velocity_init
+        with tf.GradientTape() as tape:
+            if self.mode == "integrator_training":
+                output_snap = []
+
+                for ts in range(self.n_time_step):
+                    pressure = self.poisson(input_seq_current)
+                    input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,:2], pressure])
+                    input_seq_current, update = self.explicit_update(input_seq_current)
+                    velocity_next = self.integrator([update, input_seq_current[:,:,:,:2]])
+                    input_seq_current = Concatenate(axis = -1)([velocity_next, input_seq_current[:,:,:,2:3]])
+                    input_seq_current = tf.clip_by_value(input_seq_current,0,1)
+                    output_snap.append(input_seq_current[:,:,:,:3])
+                output = Concatenate(axis = -1)(output_snap)
+                
+            else:
+                output_snap = []
+                for ts in range(self.n_time_step):
+                    pressure = self.poisson(input_seq_current)
+                    input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,:2], pressure])
+                    input_seq_current, update = self.explicit_update(input_seq_current)
+                    input_seq_current = tf.clip_by_value(input_seq_current,0,1)
+                    output_snap.append(input_seq_current[:,:,:,:3])
+                output = Concatenate(axis = -1)(output_snap)
+                
+            total_loss = tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(output,velocity_gt) 
+                           
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+
+        return {
+            "total_loss": self.total_loss_tracker.result(),
+        }
+    
+    # Update scheme
+    def explicit_update(self, input_seq_current):
+        input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
+
+        if self.solver == "rk4":
+            input_seq_current, update = self.rk4_update(input_seq_current)
+        elif self.solver == 'heun':
+            input_seq_current, update = self.heun_update(input_seq_current)
+        else:
+            input_seq_current, update = self.euler_update(input_seq_current)
+
+        return input_seq_current, update
+
+    def rk4_update(self, input_seq_current):
+
+        # Compute k1
+        k1 = self.differentiator(input_seq_current)
+
+        # Compute k2
+        inp_k2 = input_seq_current[:,:,:,1:] + self.step_size*1/2*k1
+        inp_k2 = Concatenate(axis = -1)([input_seq_current[:,:,:,0:1],inp_k2])
+
+        k2 = self.differentiator(inp_k2)
+
+        # Compute k3
+        inp_k3 = input_seq_current[:,:,:,1:] + self.step_size*1/2*k2
+        inp_k3 = Concatenate(axis = -1)([input_seq_current[:,:,:,0:1],inp_k3])
+        k3 = self.differentiator(inp_k3)
+
+        # Compute k4
+        inp_k4 = input_seq_current[:,:,:,1:] + self.step_size*k3
+        inp_k4 = Concatenate(axis = -1)([input_seq_current[:,:,:,0:1],inp_k4])
+
+        k4 = self.differentiator(inp_k4)
+
+        # Final
+        update = 1/6*(k1 + 2*k2 + 2*k3 + k4)
+        final_state = input_seq_current[:,:,:,1:] + self.step_size*update 
+        input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,0:1],final_state])
+        return input_seq_current, update
+    
+    # Euler update function
+    def heun_update(self, input_seq_current):
+        # Compute update
+        k1 = self.differentiator(input_seq_current)
+
+        # Compute k2       
+        inp_k2 = input_seq_current[:,:,:,0:2] + self.step_size*k1
+        inp_k2 = Concatenate(axis = -1)([inp_k2,input_seq_current[:,:,:,2:]])
+
+        k2 = self.differentiator(inp_k2)
+        
+        update = 1/2*(k1 + k2)
+        
+        final_state = input_seq_current[:,:,:,0:2] + self.step_size*update 
+        input_seq_current = Concatenate(axis = -1)([final_state,input_seq_current[:,:,:,2:]])
+        return input_seq_current, update
+    
+#     # Euler update function
+#     def euler_update(self, input_seq_current):
+#         # Compute update
+#         update = self.differentiator(input_seq_current)
+#         input_seq_current = input_seq_current + self.step_size*update 
+
+#         return input_seq_current, update
