c86b21dfe52138062555e18086cd1deb157d918d
Xinlun Cheng <chengxinlun@gmail.com>
2024-02-13 16:26:45 -0500

    Added Bogacki–Shampine integrator, aka matlab ode3

--- parc/model/model_hypersonic.py
+++ parc/model/model_hypersonic.py
@@ -1,8 +1,8 @@
 from tensorflow import keras
-from tensorflow.keras import  layers, regularizers
+from tensorflow.keras import layers, regularizers
 from keras.layers import *
 import tensorflow as tf
-from PARCv2.parc import layer
+from parc import layer
 
 from tensorflow.keras.layers import Concatenate, Input
 from tensorflow.keras.models import Model
@@ -68,32 +68,22 @@ def differentiator_em(n_state_var=2):
     differentiator = Model(input_tensor, output_tensor)
     return differentiator
 
+
 def integrator(n_state_var=2):
-    state_integrators = []
-    for _ in range(n_state_var):
-        state_integrators.append(layer.integrator_cnn(input_shape=DATA_SHAPE))
-
-    velocity_integrator = layer.integrator_cnn(input_shape=DATA_SHAPE, n_output=2)
-
-    state_var_prev = keras.layers.Input(shape=(DATA_SHAPE[0], DATA_SHAPE[1], n_state_var), 
+    state_var_prev = keras.layers.Input(shape=(DATA_SHAPE[0], DATA_SHAPE[1], n_state_var+2), 
                                         dtype=tf.float32)
-    velocity_prev = keras.layers.Input(shape=(DATA_SHAPE[0], DATA_SHAPE[1], 2),
+    state_var_dot = keras.layers.Input(shape=(DATA_SHAPE[0], DATA_SHAPE[1], n_state_var+2), 
                                        dtype=tf.float32)
-    
-    state_var_dot = keras.layers.Input(shape=(DATA_SHAPE[0], DATA_SHAPE[1], n_state_var), 
-                                       dtype=tf.float32)
-    velocity_dot = keras.layers.Input(shape=(DATA_SHAPE[0], DATA_SHAPE[1], 2), dtype=tf.float32)
-
-    state_var_next = []
-        
-    for i in range(n_state_var): 
-        state_var_next.append(state_integrators[i]([state_var_dot[:,:,:,i:i+1],
-                                                    state_var_prev[:,:,:,i:i+1]]))
-
-    state_var_next = keras.layers.concatenate(state_var_next, axis=-1)
-    velocity_next = velocity_integrator([velocity_dot, velocity_prev])
-    integrator = keras.Model([state_var_dot, velocity_dot, state_var_prev, velocity_prev],
-                             [state_var_next, velocity_next])
+    conv = layer.conv_block_down(state_var_dot, feat_dim=64, reps=1, kernel_size=5,
+                                 mode='down')
+    conv2 = layer.conv_block_down(conv, feat_dim=128, reps=1, kernel_size=5, 
+                                  mode='normal')
+    conv3 = layer.conv_block_up_wo_concat(conv2, feat_dim=64, reps=1, kernel_size=5,
+                                          mode='up')
+    conv_out = Conv2D(n_state_var+2, 3, padding='same')(conv3)
+    state_var_next = Add()([state_var_prev, conv_out])
+    integrator = keras.Model([state_var_dot, state_var_prev], [state_var_next], 
+                             name='integrator')
     return integrator
 
 @keras.saving.register_keras_serializable()
@@ -148,12 +138,7 @@ class PARCv2(keras.Model):
         for _ in range(self.n_time_step):    
             input_seq_current, update = self.explicit_update(input_seq_current)
             if self.use_data_driven_int == True:
-                state_var_next, velocity_next = self.integrator([update[:,:,:,:2],
-                                                                 update[:,:,:,2:],
-                                                                 input_seq_current[:,:,:,:2],
-                                                                 input_seq_current[:,:,:,2:]])
-                input_seq_current = Concatenate()([state_var_next, velocity_next])
-                        
+                input_seq_current = self.integrator([update, input_seq_current])
             res.append(input_seq_current)
         return res
 
@@ -168,19 +153,18 @@ class PARCv2(keras.Model):
 
         input_seq_current = input_seq
         with tf.GradientTape() as tape:
+            res = []
             for ts in range(self.n_time_step):
                 # Compute k1
                 input_seq_current, update = self.explicit_update(input_seq_current)
                 if self.mode == "integrator_training":
-                    state_var_next, velocity_next = self.integrator([update[:,:,:,:2],
-                                                                     update[:,:,:,2:], input_seq_current[:,:,:,:2], input_seq_current[:,:,:,2:]])
-                    input_seq_current = Concatenate()([state_var_next, velocity_next])
-
-            state_var_next = input_seq_current[:,:,:,:2]
-            velocity_next = input_seq_current[:,:,:,2:]
-                    
-            total_loss  = (tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(state_var_next,state_var_gt) + 
-                            tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(velocity_next,velocity_gt))/2
+                    input_seq_current = self.integrator([update, input_seq_current])
+                res.append(input_seq_current)
+            res = tf.stack(res, axis=1)
+            state_var_next = res[:,:,:,:,:2]
+            velocity_next = res[:,:,:,:,2:]
+            total_loss = (tf.keras.losses.MeanAbsoluteError(reduction='auto')(state_var_next,state_var_gt) + 
+                          tf.keras.losses.MeanAbsoluteError(reduction='auto')(velocity_next,velocity_gt))/2
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
 
@@ -200,18 +184,17 @@ class PARCv2(keras.Model):
         velocity_gt = tf.cast(data[1][1], dtype = tf.float32)
 
         input_seq_current = input_seq
+        res = []
         for ts in range(self.n_time_step):
             input_seq_current, update = self.explicit_update(input_seq_current)
             if self.mode == "integrator_training":
-                state_var_next, velocity_next = self.integrator([update[:,:,:,:2], update[:,:,:,2:], 
-                                                                 input_seq_current[:,:,:,:2], input_seq_current[:,:,:,2:]])
-                input_seq_current = Concatenate()([state_var_next, velocity_next])
-                
-        state_var_next = input_seq_current[:,:,:,:2]
-        velocity_next = input_seq_current[:,:,:,2:]
-                    
-        total_loss  = (tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(state_var_next,state_var_gt) + 
-                       tf.keras.losses.MeanAbsoluteError(reduction = 'sum')(velocity_next,velocity_gt))/2
+                input_seq_current = self.integrator([update, input_seq_current])
+            res.append(input_seq_current)
+        res = tf.stack(res, axis=1)
+        state_var_next = res[:,:,:,:,:2]
+        velocity_next = res[:,:,:,:,2:]            
+        total_loss  = (tf.keras.losses.MeanAbsoluteError(reduction='auto')(state_var_next,state_var_gt) + 
+                       tf.keras.losses.MeanAbsoluteError(reduction='auto')(velocity_next,velocity_gt))/2
 
         self.total_loss_tracker.update_state(total_loss)
 
@@ -221,33 +204,46 @@ class PARCv2(keras.Model):
     
     # Update scheme
     def explicit_update(self, input_seq_current):
+        # Recommend refactor into directly using Butcher table
         if self.solver == "rk4":
             input_seq_current, update = self.rk4_update(input_seq_current)
         elif self.solver == 'heun':
             input_seq_current, update = self.heun_update(input_seq_current)
+        elif self.solver == 'ode3':
+            input_seq_current, update = self.ode3_update(input_seq_current)
         else:
             input_seq_current, update = self.euler_update(input_seq_current)
 
         return input_seq_current, update
 
-    def rk4_update(self, input_seq_current):
+    def ode3_update(self, input_seq_current):
+        '''
+        Bogacki–Shampine, the same method behind Matlab solver ode3. 3rd order.
+        '''
         input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
+        k1 = self.differentiator(input_seq_current)
+        k2 = self.differentiator(input_seq_current + 1/2 * self.step_size * k1)
+        k3 = self.differentiator(input_seq_current + 3/4 * self.step_size * k2)
+        update = 2/9 * k1 + 1/3 * k2 + 4/9 * k3
+        input_seq_current = input_seq_current + self.step_size * update
+        return input_seq_current, update
 
+    def rk4_update(self, input_seq_current):
+        '''
+        Original RK method. 4th order
+        '''
+        input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
         # Compute k1
         k1 = self.differentiator(input_seq_current)
-
         # Compute k2
         inp_k2 = input_seq_current + self.step_size*1/2*k1 
         k2 = self.differentiator(inp_k2)
-
         # Compute k3
         inp_k3 = input_seq_current + self.step_size*1/2*k2
         k3 = self.differentiator(inp_k3)
-
         # Compute k4
         inp_k4 = input_seq_current + self.step_size*k3
         k4 = self.differentiator(inp_k4)
-
         # Final
         update = 1/6*(k1 + 2*k2 + 2*k3 + k4)
         input_seq_current = input_seq_current + self.step_size*update 
@@ -255,24 +251,27 @@ class PARCv2(keras.Model):
     
     # Euler update function
     def heun_update(self, input_seq_current):
+        '''
+        Heun's method. 2nd order
+        '''
         input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
-        # Compute update
+        # Compute k1
         k1 = self.differentiator(input_seq_current)
-
         # Compute k2
         inp_k2 = input_seq_current + self.step_size*k1 
         k2 = self.differentiator(inp_k2)
-        
+        # Final 
         update = 1/2*(k1 + k2)
         input_seq_current = input_seq_current + self.step_size*update 
-
         return input_seq_current, update
     
     # Euler update function
     def euler_update(self, input_seq_current):
+        '''
+        Euler's method. 1st order
+        '''
         input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
         # Compute update
         update = self.differentiator(input_seq_current)
         input_seq_current = input_seq_current + self.step_size*update 
-
         return input_seq_current, update
