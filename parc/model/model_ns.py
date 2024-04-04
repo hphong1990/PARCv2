61e7e5253f99bf476b8423813d4c19f8ea62f3e3
hphong1990 <phongnch90@gmail.com>
2024-04-04 10:57:57 -0400

    Update model_ns.py
    - Subclassing from base_model class
    - Make input_shape changeable from input
    - Cleaning small stuff

--- parc/model/model_ns.py
+++ parc/model/model_ns.py
@@ -6,80 +6,9 @@ from parc import layer
 
 from tensorflow.keras.layers import Concatenate, Input
 from tensorflow.keras.models import Model
+from parc.model.base_model import PARCv2
 
-def differentiator_ns():
-    # Model initiation
-    feature_extraction = layer.feature_extraction_unet(input_shape = (128,256), n_out_features = 64, n_base_features = 64, n_channel = 3)
-    
-    advection = [layer.Advection() for _ in range(2)]
-    diffusion = [layer.Diffusion() for _ in range(2)]
-    velocity_mapping_and_recon = layer.mapping_and_recon_cnn(input_shape = (128,256), n_base_features = 64, n_mask_channel=4, output_channel=2)
-
-    # Main computation graph
-    velocity_field = Input(shape=(128,256, 3), dtype = tf.float32)
-
-    # Reaction term
-    dynamic_feature = feature_extraction(velocity_field)
-
-    # u
-    advec_u = advection[0](velocity_field[:, :, :, 0:1], velocity_field[:, :, :, :2])
-    diffusion_u = diffusion[0](velocity_field[:, :, :, 0:1])
-    
-    # v
-    advec_v = advection[1](velocity_field[:, :, :, 1:2], velocity_field[:, :, :, :2])
-    diffusion_v = diffusion[1](velocity_field[:, :, :, 1:2])    
-    
-    # Concatenate
-    advec_diff_concat = Concatenate(axis=-1)([advec_u,advec_v,diffusion_u,diffusion_v])
-    
-    # Final mapping
-    velocity_dot = velocity_mapping_and_recon([dynamic_feature, advec_diff_concat])
-    
-    differentiator = Model(velocity_field, velocity_dot, name = 'differentiator')
-    return differentiator
-
-
-def poisson_block(input_shape = (128,256),n_base_features = 64):
-    inputs = keras.Input(shape = (input_shape[0], input_shape[1], 3), dtype = tf.float32)
-    poisson = layer.Poisson()([inputs[:,:,:,0:1],inputs[:,:,:,1:2]])
-                               
-    concat = Concatenate(axis = -1)([inputs,poisson[0], poisson[1],poisson[2]])
-    conv = layer.conv_block_down(concat,
-                           feat_dim = n_base_features,
-                            reps = 1,
-                            kernel_size = 3,
-                            mode = 'normal')
-    conv_res = layer.resnet_block(conv, n_base_features, kernel_size = 3, reps = 2, pooling = False)
-    conv_out = Conv2D(1,1, padding='same')(conv_res)
-    poisson = keras.Model([inputs], conv_out,  name = 'possion')
-    return poisson
- 
-def integrator():
-    state_var_prev = keras.layers.Input(shape = (128, 256, 2), dtype = tf.float32)
-    state_var_dot = keras.layers.Input(shape = (128, 256,2), dtype = tf.float32)
-
-    conv = layer.conv_block_down(state_var_dot,
-                           feat_dim = 64,
-                            reps = 1,
-                            kernel_size = 5,
-                            mode = 'down')
-    conv2 = layer.conv_block_down(conv,
-                           feat_dim = 128,
-                            reps = 1,
-                            kernel_size = 5,
-                            mode = 'normal')
-    conv3 = layer.conv_block_up_wo_concat(conv2,
-                           feat_dim = 64,
-                            reps = 1,
-                            kernel_size = 5,
-                            mode = 'up')
-    conv_out = Conv2D(2,3, padding='same')(conv3)
-    state_var_next = Add()([state_var_prev, conv_out])
-    integrator = keras.Model([state_var_dot, state_var_prev], [state_var_next], name = 'integrator')
-    return integrator
-
-@keras.saving.register_keras_serializable()
-class PARCv2_ns(keras.Model):
+class PARCv2_ns(PARCv2):
     def __init__(self, n_time_step, step_size, solver = "rk4", mode = "integrator_training", use_data_driven_int = True, differentiator_backbone = 'em', **kwargs):
         super(PARCv2_ns, self).__init__(**kwargs)
         self.n_time_step = n_time_step
@@ -87,29 +16,86 @@ class PARCv2_ns(keras.Model):
         self.solver = solver
         self.mode = mode
         self.use_data_driven_int = use_data_driven_int
-        self.differentiator = differentiator_ns()
-        self.poisson = poisson_block()
-        self.integrator = integrator()
+        self.differentiator = self.build_differentiator()
+        self.poisson = self.poisson_block()
+        self.integrator = self.build_integrator()
         self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
         if self.mode == "integrator_training":
             self.differentiator.trainable = False
             self.poisson.trainable = False
         else:
             self.integrator.trainable = False
-
-        self.input_layer = keras.layers.Input((128, 256, 3))
-        self.out = self.call(self.input_layer)
+       
+    def build_differentiator(self, input_shape = (128,256), n_out_features = 64, n_base_features = 64):
+        # Model initiation
+        feature_extraction = layer.feature_extraction_unet(input_shape = input_shape, n_out_features = n_out_features, n_base_features = n_base_features, n_channel = 3)
         
-        super(PARCv2_ns, self).__init__(
-            inputs= self.input_layer, outputs=self.out, **kwargs
-        )
-    
-    def build(self):
-        self._is_graph_network = True
-        self._init_graph_network(
-            inputs=[self.input_layer], outputs=self.out
-        )
+        advection = [layer.Advection() for _ in range(2)]
+        diffusion = [layer.Diffusion() for _ in range(2)]
+        velocity_mapping_and_recon = layer.mapping_and_recon_cnn(input_shape = input_shape, n_base_features = n_base_features, n_mask_channel=4, output_channel=2)
 
+        # Main computation graph
+        velocity_field = Input(shape=(input_shape[0],input_shape[1], 3), dtype = tf.float32)
+
+        # Reaction term
+        dynamic_feature = feature_extraction(velocity_field)
+
+        # u
+        advec_u = advection[0](velocity_field[:, :, :, 0:1], velocity_field[:, :, :, :2])
+        diffusion_u = diffusion[0](velocity_field[:, :, :, 0:1])
+        
+        # v
+        advec_v = advection[1](velocity_field[:, :, :, 1:2], velocity_field[:, :, :, :2])
+        diffusion_v = diffusion[1](velocity_field[:, :, :, 1:2])    
+        
+        # Concatenate
+        advec_diff_concat = Concatenate(axis=-1)([advec_u,advec_v,diffusion_u,diffusion_v])
+        
+        # Final mapping
+        velocity_dot = velocity_mapping_and_recon([dynamic_feature, advec_diff_concat])
+        
+        differentiator = Model(velocity_field, velocity_dot, name = 'differentiator')
+        return differentiator
+
+    def poisson_block(self, input_shape = (128,256),n_base_features = 64):
+        inputs = keras.Input(shape = (input_shape[0], input_shape[1], 3), dtype = tf.float32)
+        poisson = layer.Poisson()([inputs[:,:,:,0:1],inputs[:,:,:,1:2]])
+                                
+        concat = Concatenate(axis = -1)([inputs,poisson[0], poisson[1],poisson[2]])
+        conv = layer.conv_block_down(concat,
+                            feat_dim = n_base_features,
+                                reps = 1,
+                                kernel_size = 3,
+                                mode = 'normal')
+        conv_res = layer.resnet_block(conv, n_base_features, kernel_size = 3, reps = 2, pooling = False)
+        conv_out = Conv2D(1,1, padding='same')(conv_res)
+        poisson = keras.Model([inputs], conv_out,  name = 'possion')
+        return poisson
+    
+    def build_integrator(self, input_shape = (128, 256)):
+        state_var_prev = keras.layers.Input(shape = (input_shape[0], input_shape[1], 2), dtype = tf.float32)
+        state_var_dot = keras.layers.Input(shape = (input_shape[0], input_shape[1], 2), dtype = tf.float32)
+
+        conv = layer.conv_block_down(state_var_dot,
+                            feat_dim = 64,
+                                reps = 1,
+                                kernel_size = 5,
+                                mode = 'down')
+        conv2 = layer.conv_block_down(conv,
+                            feat_dim = 128,
+                                reps = 1,
+                                kernel_size = 5,
+                                mode = 'normal')
+        conv3 = layer.conv_block_up_wo_concat(conv2,
+                            feat_dim = 64,
+                                reps = 1,
+                                kernel_size = 5,
+                                mode = 'up')
+        conv_out = Conv2D(2,3, padding='same')(conv3)
+        state_var_next = Add()([state_var_prev, conv_out])
+        integrator = keras.Model([state_var_dot, state_var_prev], [state_var_next], name = 'integrator')
+        return integrator
+    
     @property
     def metrics(self):
         return [
@@ -151,7 +137,7 @@ class PARCv2_ns(keras.Model):
         with tf.GradientTape() as tape:
             if self.mode == "integrator_training":
                 output_snap = []
-                for ts in range(self.n_time_step):
+                for _ in range(self.n_time_step):
                     pressure = self.poisson(input_seq_current)
                     input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,:2], pressure])
                     input_seq_current, update = self.explicit_update(input_seq_current)
@@ -163,7 +149,7 @@ class PARCv2_ns(keras.Model):
                 
             else:
                 output_snap = []
-                for ts in range(self.n_time_step):
+                for _ in range(self.n_time_step):
                     pressure = self.poisson(input_seq_current)
                     input_seq_current = Concatenate(axis = -1)([input_seq_current[:,:,:,:2], pressure])
                     input_seq_current, update = self.explicit_update(input_seq_current)
@@ -181,18 +167,18 @@ class PARCv2_ns(keras.Model):
             "total_loss": self.total_loss_tracker.result(),
         }
     
-    # Update scheme
-    def explicit_update(self, input_seq_current):
-        input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
+    # # Update scheme
+    # def explicit_update(self, input_seq_current):
+    #     input_seq_current = tf.clip_by_value(input_seq_current, 0, 1)
 
-        if self.solver == "rk4":
-            input_seq_current, update = self.rk4_update(input_seq_current)
-        elif self.solver == 'heun':
-            input_seq_current, update = self.heun_update(input_seq_current)
-        else:
-            input_seq_current, update = self.euler_update(input_seq_current)
+    #     if self.solver == "rk4":
+    #         input_seq_current, update = self.rk4_update(input_seq_current)
+    #     elif self.solver == 'heun':
+    #         input_seq_current, update = self.heun_update(input_seq_current)
+    #     else:
+    #         input_seq_current, update = self.euler_update(input_seq_current)
 
-        return input_seq_current, update
+    #     return input_seq_current, update
 
     def rk4_update(self, input_seq_current):
 
