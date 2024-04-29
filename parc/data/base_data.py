7e52ca97a59e9be0a338c8dac2d07edce4e19795
hphong1990 <phongnch90@gmail.com>
2024-04-29 16:34:27 -0400

    Fix data_normalization_with_value not returning a value

--- parc/data/base_data.py
+++ parc/data/base_data.py
@@ -19,6 +19,7 @@ class BaseData:
         norm_data = np.zeros(input_data.shape)
         for i in range(no_of_channel):
             norm_data[:,:,:,i::no_of_channel] = ((input_data[:,:,:,i::no_of_channel] - min_val[i]) / (max_val[i] - min_val[i] + 1E-9))
+        return norm_data
 
     def data_denormalization(self, input_data, min_val, max_val, no_of_channel):
         denorm_data = np.zeros(input_data.shape)
