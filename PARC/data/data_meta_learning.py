870d7c1dc8ff48732364c357a2184133a0df4c9a
Jack <jtb3sud@virginia.edu>
2024-05-02 15:57:09 -0400

    Editing previous code to make more accessible for users

--- PARC/data/data_meta_learning.py
+++ PARC/data/data_meta_learning.py
@@ -4,17 +4,22 @@ import numpy as np
 import skimage
 from skimage.measure import block_reduce
 
-### idx range is the file range
-### sequence length is 
-### number of state variables.. maybe temp, pressure, micros?
-### purpose is the mde being used for this scenario 
   
-def clip_raw_data(idx_range, sequence_length=2, n_state_var=3, purpose = "diff_training"):
+def clip_raw_data(sequence_length=2, n_state_var=3, purpose = "diff_training", folder_path='...', image_size = (128, 208)):
+    
+    # idx_range: is used for splitting data into training and testing data, we will do this automatically in the future
+    # sequence_length: the amount of time steps contained for one sequence
+    # n_state_var: is the number of state variables our model considers - PARCv2 considers: Temperature, Pressure, and Microstructure
+    # purpose: diff_training will chop data with a sliding window of size sequence length from any starting time step; else int_training it will takes from time zero to sequence length 
+    # folder_path: folder containing the data for the simulation
+    # image_size: is the x and y dimension of the numpy data.. in tensorflow it is H and W
+    
+    
+    # init list of training samples
+    # state is for state variables
+    # vel is for velocity
     state_seq_whole = []
     vel_seq_whole = []
-
-   # Specify the path to your folder containing the files
-    folder_path = '/scratch/jtb3sud/tatb_np'
 
     # Loop through each file in the folder
     for filename in os.listdir(folder_path):
@@ -23,27 +28,39 @@ def clip_raw_data(idx_range, sequence_length=2, n_state_var=3, purpose = "diff_t
             print(f"Processing {filename}")
             raw_data = np.float32(np.load(file_path))
             data_shape = raw_data.shape
+            
+            # ensures the data will work for the sequence length
             if data_shape[2] > sequence_length:
-                ## padding was for equal size data
-                # i do not have this issue 
-                #npad = ((0, abs(data_shape[0] - 512)), (0, abs(data_shape[1] - 1024)), (0, 0))
-                #raw_data = np.pad(raw_data, pad_width=npad, mode='edge')
+                
+                # create batch dimensions for training for TensorFlow
                 raw_data = np.expand_dims(raw_data, axis=0)
+                
+                # reduce the spatial size of the height and width
                 raw_data = skimage.measure.block_reduce(raw_data[:,:,:,:], (1,4,4,1),np.max)
+                
 
                 data_shape = raw_data.shape
+                
+                # calculate the number of time steps in the original data
                 num_time_steps = data_shape[-1] // (n_state_var + 2)
+                
+                # j_range determines how the data should slide for time sequence
+                # this indicates the maximum starting index you observe from 
                 if purpose == "diff_training":
                     j_range = num_time_steps - sequence_length
                 else:
+                    # start from beginning to sequence length
                     j_range = 1
+                    
                 ### changing 128 x 208
-                state_seq_case = [np.concatenate([raw_data[:, :128, :208, (j + k) * (n_state_var + 2):\
+                
+                ### TO DO:  Diagram for explaining process 
+                state_seq_case = [np.concatenate([raw_data[:, :image_size[0], :image_size[1], (j + k) * (n_state_var + 2):\
                                                         (j + k) * (n_state_var + 2) + n_state_var] \
                                                         for k in range(sequence_length)], axis=-1) \
                                                         for j in range  (j_range)] 
 
-                vel_seq_case = [np.concatenate([raw_data[:, :128, :208, (j + k) * (n_state_var + 2) +  n_state_var :\
+                vel_seq_case = [np.concatenate([raw_data[:, :image_size[0], :image_size[1], (j + k) * (n_state_var + 2) +  n_state_var :\
                                                         (j + k) * (n_state_var + 2) + n_state_var + 2] \
                                                         for k in range(sequence_length)], axis=-1) \
                                                         for j in range (j_range)] 
@@ -62,7 +79,11 @@ def data_normalization(input_data,no_of_channel):
     norm_data = np.zeros(input_data.shape)
     min_val = []
     max_val = []
+    
+    # loop through each channel to normalize each index of the channel data
     for i in range(no_of_channel):
+        # [batch, height, width, channel]
+        # [:: refers to skipping by no_of_channels]
         norm_data[:,:,:,i::no_of_channel] = ((input_data[:,:,:,i::no_of_channel] - np.amin(input_data[:,:,:,i::no_of_channel])) / (np.amax(input_data[:,:,:,i::no_of_channel]) - np.amin(input_data[:,:,:,i::no_of_channel])) + 1E-9)
         min_val.append(np.amin(input_data[:,:,:,i::no_of_channel]))
         max_val.append(np.amax(input_data[:,:,:,i::no_of_channel]))
