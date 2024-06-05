ed5fb4b75bbc18019ce299f8a9f6cddacd3f88d9
JosephBChoi <joseph.boogun.choi@gmail.com>
2024-06-05 13:07:27 -0400

    Implement url download functionality

--- PARC/misc.py
+++ PARC/misc.py
@@ -1,5 +1,8 @@
 import numpy as np
 from scipy.ndimage import gaussian_filter
+import os
+import shlex
+
 
 def scale_temperature(temperatures, start_ts, max_temp, min_temp):
     """scale back temperature to original scales
@@ -12,6 +15,10 @@ def scale_temperature(temperatures, start_ts, max_temp, min_temp):
     """
     temperatures = temperatures[:, :, :, start_ts:]
 
-#     temperatures_scaled = (temperatures + 1.0) / 2.0
+    #     temperatures_scaled = (temperatures + 1.0) / 2.0
     temperatures_scaled = (temperatures * (max_temp - min_temp)) + min_temp
     return temperatures_scaled
+
+
+def geturl(url, save_name):
+    os.system(f"wget -O {save_name} {shlex.quote(url)}")
